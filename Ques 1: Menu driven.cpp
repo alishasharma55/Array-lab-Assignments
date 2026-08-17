d18804e8c9444ccb2bd041cc1f1ff8fa771d9f6e
alishasharma55 <alishasharma8283@gmail.com>
2026-08-17 22:46:41 +0530

    Add menu driven array program

---
+++ Ques 1: Menu driven.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100], n = 0;
+    int choice, pos, value, key;
+
+    do
+    {
+        cout << "\n----- MENU -----\n";
+        cout << "1. CREATE\n";
+        cout << "2. DISPLAY\n";
+        cout << "3. INSERT\n";
+        cout << "4. DELETE\n";
+        cout << "5. LINEAR SEARCH\n";
+        cout << "6. EXIT\n";
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter number of elements: ";
+            cin >> n;
+
+            cout << "Enter elements:\n";
+            for (int i = 0; i < n; i++)
+            {
+                cin >> arr[i];
+            }
+
+            cout << "Array created successfully.\n";
+            break;
+
+        case 2:
+            cout << "Array elements: ";
+            for (int i = 0; i < n; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+            break;
+
+        case 3:
+            cout << "Enter position (1 to " << n + 1 << "): ";
+            cin >> pos;
+
+            cout << "Enter value: ";
+            cin >> value;
+
+            if (pos < 1 || pos > n + 1)
+            {
+                cout << "Invalid position.\n";
+            }
+            else
+            {
+                for (int i = n; i >= pos; i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+
+                arr[pos - 1] = value;
+                n++;
+
+                cout << "Element inserted successfully.\n";
+            }
+            break;
+
+        case 4:
+            cout << "Enter position to delete (1 to " << n << "): ";
+            cin >> pos;
+
+            if (pos < 1 || pos > n)
+            {
+                cout << "Invalid position.\n";
+            }
+            else
+            {
+                for (int i = pos - 1; i < n - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+
+                n--;
+
+                cout << "Element deleted successfully.\n";
+            }
+            break;
+
+        case 5:
+            cout << "Enter element to search: ";
+            cin >> key;
+
+            {
+                bool found = false;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (arr[i] == key)
+                    {
+                        cout << "Element found at position "
+                             << i + 1 << endl;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    cout << "Element not found.\n";
+                }
+            }
+            break;
+
+        case 6:
+            cout << "Exiting program...\n";
+            break;
+
+        default:
+            cout << "Invalid choice.\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
+}
